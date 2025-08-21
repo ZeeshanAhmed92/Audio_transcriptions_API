8c288799fe8537e16128926d827a2ff0158527af
Zeeshanahmed92 <zeeshan.ahmed1179@gmail.com>
2025-08-21 23:32:27 +0500

    app.py updated

--- app.py
+++ app.py
@@ -554,5 +554,5 @@ if __name__ == '__main__':
     os.makedirs(TEMP_DIR, exist_ok=True)
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     os.makedirs(REPORT_FOLDER, exist_ok=True)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
